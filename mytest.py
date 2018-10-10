41ecec243d2e0606a917224682ceeedf8c431389
unknown <benam.sim@gmail.com>
2018-10-10 16:32:47 +0200

    first commit

---
+++ mytest.py
@@ -0,0 +1,24 @@
+import unittest
+import mycode as m
+
+class mytest(unittest.TestCase):
+
+    def test_hello_world():
+        self.assertEqual(m.hello(), "hello world")
+
+
+    def test_score_joseph(self):
+        self.assertEqual(m.calculer_score("Joseph", "16"), "66")
+
+    def test_score_marie(self):
+        self.assertEqual(m.calculer_score("Marie", "33"), "50")
+
+    def test_score_marc(self):
+        self.assertEqual(m.calculer_score("Marc", "60"), "43")
+
+    def test_score_ely(self):
+        self.assertEqual(m.calculer_score("Broly", "28"), "75")
+
+
+if __name__ == '__name__':
+    unittest.main()
